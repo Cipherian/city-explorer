eba671090bbed41f5d08be19aa089109012ce778
Daniel Brott <danbrott42@gmail.com>
2022-08-03 18:28:09 -0700

    copied Hexx's work and got everything running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from "react";
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      citySearch: "",
+      locationObj: {}
+    }
+  }
+
+  getLocation = async () => {
+    const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.theCityWeSearchedFor}&format=json`;
+    console.log("URL: ", url);
+    const response = await axios.get(url);
+    console.log("Response Display Name: ", response.data[0]);
+    this.setState({ locationObj: response.data[0] });
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <input 
+          onChange={(event) => this.setState({citySearch: event.target.value })} 
+          placeholder='search for a city' ></input>
+        <button onClick={this.getLocation} >Explore!</button>
+
+        {this.state.locationObj.place_id && 
+        <>
+          <h2>City we searched for: {this.state.locationObj.display_name}</h2>
+          <p>Lat/Lon: {this.state.locationObj.lat},  {this.state.locationObj.lon}</p>
+        </>
+        }
+      </div>
+    );
+  }
 }
-
 export default App;
